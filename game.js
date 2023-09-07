25cf1ef3458eafe2330b27792fb28da1c6307047
Bharat Nilam <46712950+bharatnilam@users.noreply.github.com>
2023-09-08 02:58:39 +0530

    Add buttons and listeners

--- game.js
+++ game.js
@@ -1,51 +1,47 @@
-let results = game();
+// let results = game();
 
-const counts = {};
-results.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+// const counts = {};
+// results.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
 
-if (counts['player'] == counts['computer'])
-    console.log("The match is drawn!");
-else if (counts['player'] > counts['computer'])
-    console.log("Player has won the match!");
-else
-    console.log("Computer has won the match!");
+// if (counts['player'] == counts['computer'])
+//     console.log("The match is drawn!");
+// else if (counts['player'] > counts['computer'])
+//     console.log("Player has won the match!");
+// else
+//     console.log("Computer has won the match!");
 
-console.log(counts);
+// console.log(counts);
 
+let btn = document.getElementById('rock');
+btn.addEventListener('click', playRound('rock'));
 
 function game() {
     let results = [];
-    for (i = 1; i <= 5 ; i++) {
-        console.log("Game number: " + i);
-        const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-        console.log("Player selection: " + playerSelection);
-        const computerSelection = getComputerChoice();
-        console.log("Computer selection: " + computerSelection);
-        let result = playRound(playerSelection, computerSelection);
-        results.push(result);
-        if (result == "draw")
-            console.log("The game is a draw");
-        else
-            console.log(result + " wins!");
-    }
+    // add player selection from button
+    // console.log("Player selection: " + playerSelection);
+    
+    console.log("Computer selection: " + computerSelection);
+    let result = playRound(playerSelection, computerSelection);
+    results.push(result);
+    if (result == "draw")
+        console.log("The game is a draw");
+    else
+        console.log(result + " wins!");
     return results;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
     if (playerSelection == computerSelection) {
-        alert("You selected: " + playerSelection + "\nComputer selected: " + computerSelection + "\n" + "The game is a draw!");
         return "draw";
     } else if (playerSelection == "rock") {
         let result = (computerSelection == "scissors") ? "player" : "computer" ;
-        alert("You selected: " + playerSelection + "\nComputer selected: " + computerSelection + "\n" + result + "!");
         return result;
     } else if (playerSelection == "paper") {
         let result = (computerSelection == "rock") ? "player" : "computer" ;
-        alert("You selected: " + playerSelection + "\nComputer selected: " + computerSelection + "\n" + result + "!");
         return result
     } else {
         let result = (computerSelection == "paper") ? "player" : "computer" ;
-        alert("You selected: " + playerSelection + "\nComputer selected: " + computerSelection + "\n" + result + "!");
         return result;
     }
 }
